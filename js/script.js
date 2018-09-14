76b217fbf056d441e6af57df66c8c84d7b55dd94
Natasha Shytsko <natashashitko@gmail.com>
2018-09-14 17:25:46 +0300

    Fixed gallery

--- js/script.js
+++ js/script.js
@@ -52,8 +52,9 @@ $(document).ready(function(){
         {
 //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
 //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
+            $(".filter").not('.'+value).hide();
+            $('.filter').filter('.'+value).hide();
+            $('.filter').filter('.'+value).show('1000');
 
         }
         if ($(".filter-button").removeClass("active")) {
