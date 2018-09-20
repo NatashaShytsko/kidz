601c692e6740fbf703f78f288e29b80ef25252cf
Natasha Shytsko <natashashitko@gmail.com>
2018-09-20 10:50:33 +0300

    Fixed main slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
-$(document).ready(function(){$(".main-slider").slick({infinite:!0,prevArrow:'<button class="arrow prev"><i class="icon-left-big"></i></button>',nextArrow:'<button class="arrow next"><i class="icon-right-big"></i></button>',autoplay:!0,autoplaySpeed:4e3})}),$(document).ready(function(){$(".teachers-slider").slick({slidesToShow:4,slidesToScroll:1,infinite:!0,ariableWidth:!0,prevArrow:'<button class="arrow prev"><i class="icon-left-big"></i></button>',nextArrow:'<button class="arrow next"><i class="icon-right-big"></i></button>',responsive:[{breakpoint:991,settings:{slidesToShow:2,prevArrow:'<button class="arrow prev"><i class="icon-left-big"></i></button>',nextArrow:'<button class="arrow next"><i class="icon-right-big"></i></button>',centerPadding:"40px"}},{breakpoint:480,settings:{slidesToShow:1,prevArrow:'<button class="arrow prev"><i class="icon-left-big"></i></button>',nextArrow:'<button class="arrow next"><i class="icon-right-big"></i></button>'}}]})}),$(document).ready(function(){$(".filter-button").click(function(){var t=$(this).attr("data-filter");"all"==t?$(".filter").show("1000"):($(".filter").not("."+t).hide(),$(".filter").filter("."+t).hide(),$(".filter").filter("."+t).show("1000")),$(".filter-button").removeClass("active")&&$(this).removeClass("active"),$(this).addClass("active")})}),$(document).ready(function(){$("span.navbar-toggle").click(function(t){$(this).toggleClass("active"),$("nav").toggle(),t.stopPropagation()})});
+$(document).ready(function () {
+
+    $('.main-slider').slick({
+        infinite: true,
+        prevArrow: '<button class="arrow prev"><i class="icon-left-big"></i></button>',
+        nextArrow: '<button class="arrow next"><i class="icon-right-big"></i></button>',
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+    $('.teachers-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        ariableWidth: true,
+        prevArrow: '<button class="arrow prev"><i class="icon-left-big"></i></button>',
+        nextArrow: '<button class="arrow next"><i class="icon-right-big"></i></button>',
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    prevArrow: '<button class="arrow prev"><i class="icon-left-big"></i></button>',
+                    nextArrow: '<button class="arrow next"><i class="icon-right-big"></i></button>',
+                    centerPadding: '40px'
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    prevArrow: '<button class="arrow prev"><i class="icon-left-big"></i></button>',
+                    nextArrow: '<button class="arrow next"><i class="icon-right-big"></i></button>',
+                }
+            }
+        ]
+    });
+    $(".filter-button").click(function () {
+        var value = $(this).attr('data-filter');
+        if (value == "all") {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.' + value).hide();
+            $('.filter').filter('.' + value).hide();
+            $('.filter').filter('.' + value).show('1000');
+        }
+        if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+        }
+        $(this).addClass("active");
+    });
+    $('span.navbar-toggle').click(function (e) {
+        $(this).toggleClass('active');
+        $('nav').toggle();
+        e.stopPropagation();
+    });
+});
